e2b3cdb91c0d7b9ef886920951384588a2c78c1b
bannetts <54030168+bannetts@users.noreply.github.com>
2020-03-25 14:44:30 +0800

    项目初始化

---
+++ vue.config.js
@@ -0,0 +1,30 @@
+const path = require("path")
+module.exports = {
+     devServer:{
+         proxy:{
+             "/api":{
+                target:"http://39.97.33.178",
+                changeOrange:true
+             },
+             "/ajax":{
+                 target:"http://m.maoyan.com",
+                 changeOrange:true
+             }
+         }
+     },
+     configureWebpack:{
+         resolve:{
+             alias:{
+                "@":path.join(__dirname,"./src"),
+                "@api":path.join(__dirname,"./src/api"),
+                "@assets":path.join(__dirname,"./src/assets"),
+                "@common":path.join(__dirname,"./src/common"),
+                "@components":path.join(__dirname,"./src/components"),
+                "@pages":path.join(__dirname,"./src/pages"),
+                "@router":path.join(__dirname,"./src/router"),
+                "@store":path.join(__dirname,"./src/store"),
+                "@utils":path.join(__dirname,"./src/utils"),
+             }
+         }
+     }
+}
